6822f8e0ae08d3621fa2768436abdfe6e2a6caae
Sohail Siadatnejad <sohale@gmail.com>
2018-09-14 00:07:32 +0100

    curl tests. First

---
+++ curl_tests.sh
@@ -0,0 +1,11 @@
+
+# man curl
+#
+#   -i, --include
+#          (HTTP) Include the HTTP-header in the  output.  The  HTTP-header
+#          includes  things  like  server-name, date of the document, HTTP-
+#          version and more...
+#
+
+curl 127.0.0.1:5000/acc/api/v1.0/invoices
+
