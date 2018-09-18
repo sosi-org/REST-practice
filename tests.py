0e7393ff14067669a81f716242a6fdca45af0ca8
Sohail Siadatnejad <sohale@gmail.com>
2018-09-18 01:36:13 +0100

    testing cci's test: a trivially passing test.

--- tests.py
+++ tests.py
@@ -1,14 +1,11 @@
 import unittest
 
-print("fine")
-
 # dont forget the (unittest.TestCase)
-
 class TrivialTest(unittest.TestCase):
 
     def test_fail1(self):
         """ deliberately failing test. """
-        self.assertEqual(True, False)
+        self.assertEqual(2, 2)
 
 if __name__ == '__main__':
     unittest.main()
